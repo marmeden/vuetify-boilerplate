19bb232cdb879f4e54f07389ad22e03134f5cd4c
marmeden <itsme@eneasmarin.es>
2018-10-10 16:24:26 +0200

    Final

--- src/main.js
+++ src/main.js
@@ -6,15 +6,28 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import { store } from './store'
+import firebase from 'firebase'
+import config from './dbconfig.js'
+
+firebase.initializeApp(config)
 
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+const unsubscribe = firebase.auth()
+.onAuthStateChanged((firebaseUser) => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+    created () {
+      if (firebaseUser) {
+        store.dispatch('autoSignIn', firebaseUser)
+      }
+    }
+  })
+  unsubscribe()
 })
